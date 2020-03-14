97706b3915381e04e2d6d1731f302ddb2b664013
youngAt19 <58137631+youngAt19@users.noreply.github.com>
2020-03-14 11:26:43 +0800

    Add files via upload
    dnn implemented via keras high-level API, with selu, batchNormalization and dropout

---
+++ simple_dnn_keras.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from tensorflow import keras
+import os
+
+#%%
+fashion_minst = keras.datasets.fashion_mnist
+(x_train_all, y_train_all), (x_test, y_test) = fashion_minst.load_data()
+
+x_valid, x_train = x_train_all[:5000], x_train_all[5000:]
+y_valid, y_train = y_train_all[:5000], y_train_all[5000:]
+
+
+'''
+Normalization
+convert np.int into float32
+x_train:[None, 28, 28] -> [None, 784] since fit_transform need 2-D parameter
+fit_transform vs transform: 
+    the former can record the mean and variance of the train data
+'''
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(
+        x_train.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28)
+x_valid_scaled = scaler.transform(
+        x_valid.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28)
+x_test_scaled = scaler.transform(
+        x_test.astype(np.float32).reshape(-1, 1)).reshape(-1, 28, 28)
+
+#%%
+def show_single_image(img_arr):
+    plt.imshow(img_arr, cmap='binary')
+    plt.show()
+
+def show_imgs(n_rows, n_cols, x_data, y_data, class_name):
+    assert len(x_data) == len(y_data)
+    assert n_rows * n_cols < len(x_data)
+    plt.figure(figsize=(n_cols * 1.4, n_rows * 1.6))
+    for row in range(n_rows):
+        for col in range(n_cols):
+            index = n_cols * row + col
+            plt.subplot(n_rows, n_cols, index+1)
+            plt.imshow(x_data[index], cmap='binary', interpolation='nearest')
+            plt.axis('off')
+            plt.title(class_name[y_data[index]])
+    plt.show()
+
+class_names = ['T-shirt', 'Trouser', 'Pullover', 'Dress',
+               'Coat', 'Sandal', 'Shirt', 'Sneaker',
+               'Bag', 'Ankle boot']
+
+#%%
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+
+'''
+#batchNormalization relief the vanishing gradient
+for _ in range(20):
+    model.add(keras.layers.Dense(100, activation='relu'))
+    model.add(keras.layers.BatchNormalization())
+
+'''
+#activation using selu contains normalization, so need not BatchNormalization
+for _ in range(20):
+    model.add(keras.layers.Dense(100, activation='selu'))
+#alphaDropout: mean, variance and normalization property unchange
+model.add(keras.layers.AlphaDropout(rate=0.5))
+
+"""
+#Another sequence of Activation and Normalization
+model.add(keras.layers.Dense(100))
+model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.Activation('relu'))
+"""
+model.add(keras.layers.Dense(10, activation='softmax'))
+#relu: y = max(0, x)
+
+
+model.compile(loss='sparse_categorical_crossentropy', 
+              optimizer='adam',
+              metrics=['accuracy'])
+
+#%%
+#TensorBoard, earlyStopping, ModelCheckpoint
+logdir = os.path.join('.\\dnn-selu_drop-callbacks')
+if not os.path.exists(logdir):
+    os.mkdir(logdir)
+output_model_file = os.path.join(logdir, 'fashion-mnist_model.h5')
+
+callbacks = [
+        keras.callbacks.TensorBoard(log_dir=logdir),
+        keras.callbacks.ModelCheckpoint(output_model_file,
+                                        save_best_only=True),
+        keras.callbacks.EarlyStopping(patience=5, min_delta=1e-3),
+        ]
+
+#%%
+history = model.fit(x_train_scaled, y_train, epochs=10,
+                    validation_data=(x_valid_scaled, y_valid),
+                    callbacks=callbacks)
+
+#%%
+def plot_learning_curves(history):
+    pd.DataFrame(history.history).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.gca().set_ylim(0, 1)
+    plt.show()
+
+plot_learning_curves(history)
+
+#%%
+model.evaluate(x_test_scaled, y_test)
